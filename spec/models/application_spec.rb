7e158252f475349dbbb746c6777bd4a6b2f9f8bb
Sherman Au <shermau13@gmail.com>
2022-04-05 19:14:24 -0600

    test: .approved for ApplicationModel

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -31,5 +31,12 @@ RSpec.describe Application, type: :model do
       @application1.update_status_pending
       expect(@application1.status).to eq('Pending')
     end
+
+    it 'updates status to approved' do
+      @application1.update_status_pending
+      expect(@application1.status).to eq('Pending')
+      @application1.approved
+      expect(@application1.status).to eq('Approved')
+    end
   end
 end
